ab6a35ce18c2438d195d2db404c77f9aa1dbbffc
CUNET\Jonathangjackson <jonathangjackson@cmail.carleton.ca>
2019-11-26 16:32:47 -0500

    Mail Mutt Mania is the New Unity Project

---
+++ MailMuttMaina/Assets/Scripts/Movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class Movement : MonoBehaviour
+{
+    private int sourcePos;
+    public float speed;
+    public Transform[] waypoints;
+    private Quaternion endPoint;
+    private int count;
+    private float distanceError = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        //endPoint = waypoints[0].rotation;
+    }
+
+    public void setSpeed(float s)
+    {
+        this.speed = s;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.right * Time.deltaTime * speed;
+        //float dis = Vector3.Distance(transform.position, waypoints[count].position);
+        float step = speed * Time.deltaTime;
+        /*
+        if(dis < distanceError)
+        {
+            Debug.Log("IN");
+            count++;
+            if (count < waypoints.Length)
+                endPoint = waypoints[count].rotation;
+            else
+                Debug.Log("End of Waypoints");
+        }*/
+        //transform.rotation = Quaternion.Slerp(transform.rotation, endPoint, 0.5f * Time.deltaTime);
+    }
+}
+
---
+++ MailMuttMaina/Assets/Scripts/Translate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Translate : MonoBehaviour
+{
+
+    private float z;
+    // Start is called before the first frame update
+    void Start()
+    {
+        z = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("d") && z < 20)
+        {
+            transform.position += transform.forward * Time.deltaTime * -10;
+            //this.GetComponent<Transform>().position = new Vector3(this.transform.position.x , this.transform.position.y, this.transform.position.z - (10 * Time.deltaTime));
+            z++;
+        }
+        if (Input.GetKey("a") && z > -20)
+        {
+            z--;
+
+            transform.position += transform.forward * Time.deltaTime * 10;
+            //this.GetComponent<Transform>().position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + (10 * Time.deltaTime));
+        }
+    }
+}
