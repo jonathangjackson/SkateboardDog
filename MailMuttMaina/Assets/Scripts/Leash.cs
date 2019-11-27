9fbeeff28c8b53ac8cafd072805df0d5e2974407
Nick <nickchojnacki24@gmail.com>
2019-11-26 22:46:54 -0500

    Implemented spawning mechanic and leash mechanic

--- MailMuttMaina/Assets/Scripts/Leash.cs
+++ MailMuttMaina/Assets/Scripts/Leash.cs
@@ -16,7 +16,7 @@ public class Leash : MonoBehaviour
     private LineRenderer leashLine;
     private float nextFire;
 
-    private GameObject currentDog;
+    public GameObject currentDog;
 
     // Start is called before the first frame update
     void Start()
@@ -42,10 +42,15 @@ public class Leash : MonoBehaviour
             {
                 leashLine.SetPosition(1, hit.point);
 
-                if (hit.transform.gameObject.tag == "Dog")
+                if (hit.transform.gameObject.tag == "NewDog")
                 {
-                    GetComponent<SpringJoint>().connectedBody = hit.rigidbody;
+                    hit.transform.gameObject.transform.position = currentDog.transform.position;
+                    hit.transform.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+                    hit.transform.gameObject.transform.parent = currentDog.transform;
+                    GetComponent<SpringJoint>().connectedBody = currentDog.GetComponent<Rigidbody>();
+                    hit.transform.gameObject.tag = "Dog";
                 }
+                
             }
             else
             {
---
+++ MailMuttMaina/Assets/Scripts/SpawnDogs.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDogs : MonoBehaviour
+{
+
+    public Transform SpawnPoint;
+    public GameObject prefab;
+
+    void OnTriggerEnter()
+    {
+        GameObject newDog = Instantiate(prefab, SpawnPoint.position, SpawnPoint.rotation);
+        newDog.gameObject.tag = "NewDog";
+
+        newDog.AddComponent<Animation>();
+        newDog.AddComponent<Animator>();
+
+        Animator animator = newDog.GetComponent<Animator>();
+
+        animator.runtimeAnimatorController = Resources.Load("Animations/Dog2") as RuntimeAnimatorController;
+
+        newDog.GetComponent<Animation>().GetComponent<Animation>().Play("Dog Run");
+
+    }
+}
