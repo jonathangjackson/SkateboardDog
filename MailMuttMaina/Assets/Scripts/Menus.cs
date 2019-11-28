675c3eec586e253426b300acdec73bd44d4cecab
Leyya Alsaadi <leyaleealsaadi@cmail.carleton.ca>
2019-11-27 19:30:37 -0500

    menus, besides the end screen, have been implemented

---
+++ MailMuttMaina/Assets/Scripts/Menus.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menus : MonoBehaviour
+
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("CreditsScreen");
+    }
+
+    public void HowToPlay()
+    {
+        SceneManager.LoadScene("HowToPlayScreen");
+
+    }
+
+    public void ReturnToStartMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+}
