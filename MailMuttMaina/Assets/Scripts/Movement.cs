c6dc8d0cf3f5c333e704383d4040eae772819dcf
CUNET\leyaleealsaadi <leyaleealsaadi@cmail.carleton.ca>
2019-12-04 10:40:19 -0500

    lose and win screen implemented

--- MailMuttMaina/Assets/Scripts/Movement.cs
+++ MailMuttMaina/Assets/Scripts/Movement.cs
@@ -13,6 +13,7 @@ public class Movement : MonoBehaviour
     private Quaternion endPoint;
     private int count;
     private float distanceError = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ MailMuttMaina/Assets/Scripts/RoadSpawning.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadSpawning : MonoBehaviour
+{
+
+    public GameObject roadA;
+    public GameObject roadB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.tag == "roadSpawn"){
+
+        }
+    }
+}
